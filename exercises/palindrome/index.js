5b5f1adf63f8efc8f59b60d22f16217f09d6dfb3
Isaac Raja <isaacraja@gmail.com>
2018-04-05 21:25:06 +0530

    Palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,17 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  str.split('').every((char, i) => {
+    return char === str[str.length-i-1];
+  })
+}
 
+// function palindrome(str) {
+//   const reversed = str.split('')
+//         .reduce((reversed, char) => {
+//           return char + reversed;
+//         });
+//   return str === reversed;
+// }
 module.exports = palindrome;
