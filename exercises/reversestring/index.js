0d297a62c6b9bd713630d90b22352d54178d3502
Isaac Raja <isaac.raja@compassitesinc.com>
2018-04-05 11:47:32 +0530

    Multiple Reverse functions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,33 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse (str) {
+//   return str.split('')
+//     .reverse()
+//     .join('')
+// }
 
-module.exports = reverse;
+// function reverse (str) {
+//   var reversed = '';
+//   for (var i = 0; i <= str.length - 1; i++) {
+//     reversed = str[i] + reversed;
+//   }
+//   return reversed;
+// }
+
+// function reverse (str) {
+//   var reversed = '';
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//   return reversed;
+// }
+
+function reverse (str) {
+  return str.split('')
+    .reduce((reversed, char) => {
+      return char + reversed;
+    })
+}
+
+module.exports = reverse
