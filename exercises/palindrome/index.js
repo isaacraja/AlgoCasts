4651b7424d84259c6f4fdaa62cf8dd5c8a989815
Isaac Raja <isaac.raja@compassitesinc.com>
2018-04-16 11:10:00 +0530

    palindrome reduce version

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,12 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  var reversed = str.split('')
+      .reduce((reversed, char) => {
+        return char + reversed;
+      })
+  return str === reversed;
+}
 
 module.exports = palindrome;
