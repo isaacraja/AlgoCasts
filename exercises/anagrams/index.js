e8638262073d3a7b3b615dcaa3ac51d937c67e75
Isaac Raja <isaacraja@gmail.com>
2018-04-17 21:42:55 +0530

    Anagram - Rough solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,31 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let strA = stringA.replace(/[^\w]/g, "").toLowerCase();
+  let strB = stringB.replace(/[^\w]/g, "").toLowerCase();
+
+  if (strA.length !== strB.length) {
+    return false;
+  }
+
+  const strMapA = buildStrMap(strA);
+  const strMapB = buildStrMap(strB);
+
+  for (let i = 0; i < Object.keys(strMapA).length; i++) {
+    if (strMapA[strA[i]] !== strMapB[strA[i]]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function buildStrMap(str) {
+  let strMap = {};
+  for (let i = 0; i < str.length; i++) {
+    strMap[str[i]] = strMap[str[i]] + 1 || 1;
+  }
+  return strMap;
+}
 
 module.exports = anagrams;
